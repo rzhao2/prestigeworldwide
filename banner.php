4c89480a767dce31a3c4f057b3ba4161c3e485e8
Matt <mdemart2@u.rochester.edu>
2013-11-15 14:43:24 -0500

    Added banner.php to use as php include() code

---
+++ banner.php
@@ -0,0 +1,27 @@
+<div id="banner">
+				<div id="banner_left">
+                    <img src="images/logo.png"><!-- logo -->
+				</div>
+				<div id="banner_right">
+					<div id="login">
+						<form name="login_form"><!--method="POST" action="process login php"-->
+							<table id="login_table">
+								<tr>
+									<td colspan="2">Login with your University of Rochester credentials.</td>
+								</tr>
+								<tr>
+									<td class="align_right">Net ID:</td>
+									<td class="align_left"><input type="text" class="login_input" /></td>
+								</tr>
+								<tr>
+									<td class="align_right">Password:</td>
+									<td class="align_left"><input type="password" class="login_input" /></td>
+								</tr>
+								<tr>
+									<td colspan="2" class="align_right"><button type="button" id="login_submit">Login</button></td>
+								</tr>
+							</table> 
+						</form>
+					</div><!-- end login -->
+				</div><!-- end banner right -->
+			</div><!-- end banner -->
