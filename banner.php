b264dff40b0865e25feb2214579d070aab3b3c5a
Matt <mdemart2@u.rochester.edu>
2013-11-15 15:27:19 -0500

    Fixed Login (hopefully!)

--- banner.php
+++ banner.php
@@ -5,7 +5,7 @@
 				<div id="banner_right">
 					<?php if(!isset($_SESSION['username'])){ ?>
 					<div id="login">
-						<form name="login_form"><!--method="POST" action="process login php"-->
+						<form name="login_form" method = "POST" action ="login.php"><!--method="POST" action="process login php"-->
 							<table id="login_table">
 								<tr>
 									<td colspan="2">Login with your University of Rochester credentials.</td>
@@ -19,17 +19,16 @@
 									<td class="align_left"><input type="password" class="login_input" name = "password"/></td>
 								</tr>
 								<tr>
-									<td colspan="2" class="align_right"><button type="button" id="login_submit" method = "POST" action ="login.php">Login</button></td>
+									<td colspan="2" class="align_right"><button type="submit" id="login_submit">Login</button></td>
 								</tr>
 							</table> 
 						</form>
 					</div><!-- end login -->
 					<?php }else{?>
-					<p>
+					<div>
 						Welcome, <?php echo($_SESSION['username']) ?>! <br />
-						<button type="button" id="login_submit" method = "POST" action ="logout.php">Logout</button></td>
-
-					</p>
+						<button type="submit" id="login_submit" method = "POST" action ="logout.php">Logout</button></td>
+					</div>
 
 					<?php }?>
 				</div><!-- end banner right -->
