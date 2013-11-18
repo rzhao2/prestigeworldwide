b58bee4366f010e80f11f58daaff591f04e673a8
Matt <mdemart2@u.rochester.edu>
2013-11-18 18:40:19 -0500

    Edited simpleCart.js to redirect to checkout.php on checkout, passing items in POST.

---
+++ checkout.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+<p>
+Checkout stuff goes here!
+<a href="orderform.php?location=connections">Go back</a>
+</p>
+</body>
+
+</html>
--- simpleCart.js
+++ simpleCart.js
@@ -87,7 +87,16 @@
 
 				// default options
 				settings = {
-					checkout				: { type: "PayPal", email: "you@yours.com" },
+				checkout: { type: "SendForm",
+	                         url: "checkout.php",
+	                      method: "POST",
+	                  extra_data: {
+	                  				/*
+	                  				  location: "connections"
+	                  				  userid: ""
+	                  				*/
+	                              }
+	                     },
 					currency				: "USD",
 					language				: "english-us",
 
