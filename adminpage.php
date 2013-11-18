de47b2dafc88ec59faeb61179cb7c4118affc74a
mdemart2 <mdemart2@u.rochester.edu>
2013-11-17 20:28:43 -0500

    Moved images to images folder

--- adminpage.php
+++ adminpage.php
@@ -5,13 +5,13 @@
 <div id='left'>
 	<div id='left_inner'>
 		<h3> Update Food </h3>
-		<a href='database_admin.php'><img height="200px" src="foodupdate.png"/></a>
+		<a href='database_admin.php'><img height="200px" src="images/foodupdate.png"/></a>
 	</div><!--end left_inner -->
 </div><!-- end left -->
 
 <div id='right'>
 	<div id='right_inner'>
 		<h3> Order Queue </h3>
-		<a href='orderqueue.php'><img height="200px" src="orderqueue.png"/></a>
+		<a href='orderqueue.php'><img height="200px" src="images/orderqueue.png"/></a>
 	</div><!--end right_inner -->
 </div><!-- end right -->
