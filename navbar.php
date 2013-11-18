0e2bfd9976a7c42a8610bcff6c6c568e6b83187d
mdemart2 <mdemart2@u.rochester.edu>
2013-11-17 23:39:01 -0500

    FIXED THE ANNOYING GRAY BAR
    The #header p had a margin of 50px which messed up the bar

--- navbar.php
+++ navbar.php
@@ -1,3 +1,6 @@
-			<nav id="nav_bar">
-                <a href="">Home</a> | <a href="final_report.php">About Us</a> | <a href="">Menus</a> | <a href="">Nutritional Info</a>
-            </nav><!-- end nav_bar -->
+<nav id="nav_bar">
+    <a href="">Home</a> |
+    <a href="final_report.php">About Us</a> |
+    <a href="">Menus</a> |
+    <a href="">Nutritional Info</a>
+</nav><!-- end nav_bar -->
