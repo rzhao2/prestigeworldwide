080ac371b3a4164123cbdc7652c334b847f7ffd2
csohmer <ccsohmer@gmail.com>
2013-11-19 23:19:16 -0500

    logos view?

--- cafechooser.php
+++ cafechooser.php
@@ -4,13 +4,13 @@
 
 <div id='left'>
 	<div id='left_inner'>
-		<a href='orderform.php?location=connections'><img height="500px" src="images/ConnectionsLogo.png" /></a>
+		<a href='orderform.php?location=connections'><img src="images/ConnectionsLogo.png" /></a>
 	</div><!--end left_inner -->
 </div><!-- end left -->
 
 <div id='right'>
 	<div id='right_inner'>
 		<h3> Coming Soon </h3>
-		<a href='orderform.php?location=puravida'><img height="200px" src="images/PuraVidaLogo.png" /></a>
+		<a href='orderform.php?location=puravida'><img src="images/PuraVidaLogo.png" /></a>
 	</div><!--end right_inner -->
 </div><!-- end right -->
