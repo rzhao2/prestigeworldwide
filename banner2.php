a3fe4fbb3f0cae7c8682ffd1cdb31b29b817524a
csohmer <ccsohmer@gmail.com>
2013-11-22 13:05:13 -0500

    test banner file

--- banner2.php
+++ banner2.php
@@ -1,5 +1,30 @@
 <div id="banner">
-        <div id="banner_left">
+  <div class="container clearfix">
+    <div class="row">
+      <div class="span12">
+        <div class="navbar navbar_">
+          <div class="container">
+            <h1 class="brand brand_"><a href="index.html"><img alt="" src="images/logo.png"> </a></h1>
+            <a class="btn btn-navbar btn-navbar_" data-toggle="collapse" data-target=".nav-collapse_">Menu <span class="icon-bar"></span> </a>
+            <div class="nav-collapse nav-collapse_  collapse">
+              <ul class="nav sf-menu">
+                <li class="active"><a href="index.php">Home</a></li>
+                <li class="sub-menu"><<a href="final_report.php">About Us</a></li>
+                <li><a href="orderform.php">Menus</a></li>
+                <a href="http://www.campusdish.com/en-US/CSNE/Rochester/Locations/Connections.htm">Nutritional Info</a>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+		
+		
+		
+		
+		
+		<div id="banner_left">
         <a href = "index.php"><img src="images/logo.png"></a><!-- logo -->
         </div>
         <div id="banner_right">
