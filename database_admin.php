59278f4ba3d7bed9814a66dc4615caf7511ce228
tteumer <tteumer2@u.rochester.edu>
2013-11-23 16:15:12 -0500

    updated for edit/new/delete item

--- database_admin.php
+++ database_admin.php
@@ -4,6 +4,8 @@ if($_SESSION['username'] != "admin")
 {
 	header('Location: index.php');	
 }
+
+$img_dir = 'images/food_images/';
 ?>
 
 <!DOCTYPE html>
@@ -13,16 +15,16 @@ if($_SESSION['username'] != "admin")
 		<link rel="stylesheet" type="text/css" href="styles.css">
         <script src="script.js"></script>
         <script>
-        function checkForm(){
-        	var myForm = document.getElementById('searchForm');
-        	if(myForm.getElementsByTagName("input")[0].value == "")
-        	{
-        		alert("Enter something in the search field!");
-        		return false;
-        	}else{
-        		return true;
-        	}
-        }
+	        function checkForm(){
+	        	var myForm = document.getElementById('searchForm');
+	        	if(myForm.getElementsByTagName("input")[0].value == "")
+	        	{
+	        		alert("Enter something in the search field!");
+	        		return false;
+	        	} else {
+	        		return true;
+	        	}
+	        }
         </script>
 	</head>
 	<body>
@@ -37,9 +39,12 @@ if($_SESSION['username'] != "admin")
 				
 			?>
 				
-			<form id = "searchForm" onsubmit="return checkForm()" action="database_admin.php" method="get">
+			<form id="searchForm" onsubmit="return checkForm()" action="database_admin.php" method="get">
 				<input type="search" name="q" />
 				<button type="submit">Submit</button>
+			</form>
+			<form id="createNewItem" action="database_admin.php" method="POST">
+				<button type="submit" name="create_new_item">Create new item</button>
 			</form>
 			<br />
 				
@@ -68,10 +73,17 @@ if($_SESSION['username'] != "admin")
 							echo '<span>Category</span>: ' . $row[2] . '<br/ >';
 							echo '<span>Description</span>: ' . $row[3] . '<br/ >';
 							echo '<span>Price</span>: ' . $row[4] . '<br/ >';
+							echo '<span>Options</span>: ' . $row[5] . '<br/ >';
+							echo '<span>Image</span>: ' . $row[6] . '<br/ >';
+							echo '<img src="' . $img_dir . $row[6] . '" alt="' . $row[6] . '" height="60px" /><br/ >';
 							echo '
 							<form action="edit_item.php" method="POST">
-								<input type="hidden" name="item_id" value=" ' . $row[0] . ' " />
+								<input type="hidden" name="item_id" value="' . $row[0] . '" />
 								<input type="submit" value="EDIT RECORD" />
+							</form>
+							<form action="delete_item.php" method="POST">
+								<input type="hidden" name="item_id" value="' . $row[0] . '" />
+								<input type="hidden" name="item_name" value="' . $row[1] . '" />
 								<input type="submit" value="DELETE RECORD" />
 							</form>
 							';
@@ -87,6 +99,9 @@ if($_SESSION['username'] != "admin")
 					if(!isset($hasResults)) { echo 'No items found matching string ' . $q; }		
 					
 				} // end outer if
+				elseif (isset($_POST['create_new_item'])) {
+					include 'create_new_item.php';
+				}
 				else // else ($q is not set)
 				{
 					$rows = mysqli_num_rows($result);
@@ -98,15 +113,22 @@ if($_SESSION['username'] != "admin")
 
 			<?php
 						$row = mysqli_fetch_row($result);
+
 						echo '<span>ID</span>: ' . $row[0] . '<br/ >';
 						echo '<span>Name</span>: ' . $row[1] . '<br/ >';
 						echo '<span>Category</span>: ' . $row[2] . '<br/ >';
 						echo '<span>Description</span>: ' . $row[3] . '<br/ >';
 						echo '<span>Price</span>: ' . $row[4] . '<br/ >';
+						echo '<span>Options</span>: ' . $row[5] . '<br/ >';
+						echo '<span>Image</span>: ' . $row[6] . '<br/ >';
 						echo '
 						<form action="edit_item.php" method="POST">
-							<input type="hidden" name="item_id" value=" ' . $row[0] . ' " />
+							<input type="hidden" name="item_id" value="' . $row[0] . '" />
 							<input type="submit" value="EDIT RECORD" />
+						</form>
+						<form action="delete_item.php" method="POST">
+							<input type="hidden" name="item_id" value="' . $row[0] . '" />
+							<input type="hidden" name="item_name" value="' . $row[1] . '" />
 							<input type="submit" value="DELETE RECORD" />
 						</form>
 						';
