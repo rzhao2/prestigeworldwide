fedd9717ff0b51506d7f16a7add23408fc9ee00b
csohmer <ccsohmer@gmail.com>
2013-12-03 18:09:21 -0500

    cafe photos and homepage edits

--- cafechooser.php
+++ cafechooser.php
@@ -1,13 +1,33 @@
 <div class="centered" id="cafeChooser">
 
 	<p>Choose your cafe</p>
-	<table class="centered" table border="0">
+	<table table border="0">
 		<tr>
-			<td class="centered" id='left-home'>
+			<td>
 				<a href='orderform.php?location=connections'><img src="images/ConnectionsLogo.png" /></a>
 			</td>
-			<td class="centered" id='right-home'>
-				<a href='orderform.php?location=puravida'><img src="images/PuraVidaLogo.png" /></a>
+			<td>
+				<img src="images/connections1.png"/>
+			</td>
+			<td>
+				<img src="images/connections2.png"/>
+			</td>
+			<td>
+				<img src="images/connections3.png"/>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<!--coming soon  <a href='orderform.php?location=puravida'>--><img src="images/PuraVidaLogo.png" /></a>
+			</td>
+			<td>
+				<img src="images/puravida1.png"/>
+			</td>
+			<td>
+				<img src="images/puravida2.png"/>
+			</td>
+			<td>
+				<img src="images/puravida3.png"/>
 			</td>
 		</tr>
 	</table>
