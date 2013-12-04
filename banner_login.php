e15421c02e0a97e44deebdf357aca2a7b07b72ce
tteumer <tteumer2@u.rochester.edu>
2013-12-04 13:01:25 -0500

    created placeholders and formatted code

--- banner_login.php
+++ banner_login.php
@@ -1,37 +1,40 @@
-<?php  if(!isset($_SESSION['username'])){ ?>
-                <div id="login">
-                        <form name="login_form" method ="POST" action ="login.php"><!--method="POST" action="process login php"-->
-                                <table id="login_table">
-                                        <tr>
-                                                <td colspan="3">Login with your University of Rochester credentials.</td>
-                                        </tr>
-                                        <tr>
-                                                <td class="align_left"><input type="text" class="login_input" name = "username" value="Net ID"/></td>
-												<td class="align_left"><input type="password" class="login_input" name = "password" value="Password"/></td>
-												<td class="align_right"><button type="submit" id="login_submit">Login</button></td>
-                                        </tr>
-                                </table>
-                        </form>
-                </div><!-- end login -->
 <?php 
-	} else { //logged in user
-		
-		if( $_SESSION['username'] != "admin") {
+if(!isset($_SESSION['username'])) { 
 ?>
-                        <form name="logout_form" method ="POST" action ="logout.php">
-                                Welcome, <?php  echo($_SESSION['username']); ?>! <br />
-                                <button type="submit" id="login_submit" method = "POST" action ="logout.php">Logout</button>
-                        </form>
-<?php 
-		} else { //admin
-?>
-                        <form name="logout_form" method ="POST" action ="logout.php">
-                                Welcome, <?php  echo($_SESSION['username']); ?>! <br />
-                                <button type="submit" id="login_submit" method = "POST" action ="logout.php">Logout</button>
-                        </form>
-<?php 
-		}
+        <div id="login">
+                <form name="login_form" method ="POST" action ="login.php">
+                        <table id="login_table">
+                                <tr>
+                                        <td colspan="3">Login with your University of Rochester credentials.</td>
+                                </tr>
+                                <tr>
+                                        <td class="align_left"><input type="text" class="login_input" name="username" value="Net ID" placeholder="Net ID"/></td>
+					<td class="align_left"><input type="password" class="login_input" name="password" value="Password" placeholder="Password"/></td>
+					<td class="align_right"><button type="submit" id="login_submit">Login</button></td>
+                                </tr>
+                        </table>
+                </form>
+        </div><!-- end login -->
 
+<?php 
+} else { //logged in user
+        if( $_SESSION['username'] != "admin") {
+?>
+
+        <form name="logout_form" method ="POST" action ="logout.php">
+                Welcome, <?php  echo($_SESSION['username']); ?>! <br />
+                <button type="submit" id="login_submit" method = "POST" action ="logout.php">Logout</button>
+        </form>
+
+<?php 
+        } else { //admin
+?>
+        <form name="logout_form" method ="POST" action ="logout.php">
+                Welcome, <?php  echo($_SESSION['username']); ?>! <br />
+                <button type="submit" id="login_submit" method = "POST" action ="logout.php">Logout</button>
+        </form>
+
+<?php 
 	}
-
+}
 ?>
