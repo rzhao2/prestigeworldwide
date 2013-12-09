4885a602d8a5dccf5f35f9bdfac829165e4b75c3
rzhao2 <rzhao2@u.rochester.edu>
2013-12-09 07:54:01 -0800

    make login enforced by database
    only crendential right now is 8388607, if u login with anything else u
    will be directed to the admin page.

--- login.php
+++ login.php
@@ -1,7 +1,25 @@
 <?php
 //test
 session_start();
-$_SESSION['username'] = $_POST['username'];
+
+$id = $_POST['username'];
+
+include 'db_connect.php'; 
+$query = "SELECT * FROM Student_info WHERE student_id = $id";
+$result = mysqli_query($db, $query);
+if(!$result) { 
+
+	$_SESSION['username'] = "admin";
+
+}
+else
+{
+	$row = mysqli_fetch_row($result);
+	$name = $row[2];
+
+	$_SESSION['username'] = $name;
+	$_SESSION['id'] = $id;
+}
 //echo "success!";
 header('Location: index.php');
 ?>
